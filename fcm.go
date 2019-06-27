a76d324431f7ebc3e33d87aaaa525819ff8eeea4
Rami <me@rami-dabain.com>
2019-06-27 23:07:53 +0200

    removed stuff

--- fcm.go
+++ fcm.go
@@ -37,8 +37,7 @@ var (
 
 // FcmClient stores the key and the Message (FcmMsg)
 type FcmClient struct {
-	ApiKey  string
-	Message FcmMsg
+	ApiKey string
 }
 
 // FcmMsg represents fcm request message
@@ -95,71 +94,22 @@ func NewFcmClient(apiKey string) *FcmClient {
 	return fcmc
 }
 
-// NewFcmTopicMsg sets the targeted token/topic and the data payload
-func (this *FcmClient) NewFcmTopicMsg(to string, body map[string]string) *FcmClient {
-
-	this.NewFcmMsgTo(to, body)
-
-	return this
-}
-
-// NewFcmMsgTo sets the targeted token/topic and the data payload
-func (this *FcmClient) NewFcmMsgTo(to string, body interface{}) *FcmClient {
-	this.Message.To = to
-	this.Message.Data = body
-
-	return this
-}
-
-// SetMsgData sets data payload
-func (this *FcmClient) SetMsgData(body interface{}) *FcmClient {
-
-	this.Message.Data = body
-
-	return this
-
-}
-
-// NewFcmRegIdsMsg gets a list of devices with data payload
-func (this *FcmClient) NewFcmRegIdsMsg(list []string, body interface{}) *FcmClient {
-	this.newDevicesList(list)
-	this.Message.Data = body
-
-	return this
-
-}
-
-// newDevicesList init the devices list
-func (this *FcmClient) newDevicesList(list []string) *FcmClient {
-	this.Message.RegistrationIds = make([]string, len(list))
-	copy(this.Message.RegistrationIds, list)
-
-	return this
-
-}
-
-// AppendDevices adds more devices/tokens to the Fcm request
-func (this *FcmClient) AppendDevices(list []string) *FcmClient {
-
-	this.Message.RegistrationIds = append(this.Message.RegistrationIds, list...)
-
-	return this
-}
-
 // apiKeyHeader generates the value of the Authorization key
 func (this *FcmClient) apiKeyHeader() string {
 	return fmt.Sprintf("key=%v", this.ApiKey)
 }
 
 // sendOnce send a single request to fcm
-func (this *FcmClient) sendOnce() (*FcmResponseStatus, error) {
+func (this *FcmClient) sendOnce(msg FcmMsg) (*FcmResponseStatus, error) {
 
 	fcmRespStatus := new(FcmResponseStatus)
 
-	jsonByte, err := this.Message.toJsonByte()
+	jsonByte, err := msg.toJsonByte()
 	if err != nil {
 		return fcmRespStatus, err
 	}
+
+	println(string(jsonByte))
 
 	request, err := http.NewRequest("POST", fcmServerUrl, bytes.NewBuffer(jsonByte))
 	request.Header.Set("Authorization", this.apiKeyHeader())
@@ -196,8 +146,8 @@ func (this *FcmClient) sendOnce() (*FcmResponseStatus, error) {
 }
 
 // Send to fcm
-func (this *FcmClient) Send() (*FcmResponseStatus, error) {
-	return this.sendOnce()
+func (this *FcmClient) Send(msg FcmMsg) (*FcmResponseStatus, error) {
+	return this.sendOnce(msg)
 
 }
 
@@ -216,100 +166,6 @@ func (this *FcmResponseStatus) parseStatusBody(body []byte) error {
 	}
 	return nil
 
-}
-
-// SetPriority Sets the priority of the message.
-// Priority_HIGH or Priority_NORMAL
-func (this *FcmClient) SetPriority(p string) *FcmClient {
-
-	if p == Priority_HIGH {
-		this.Message.Priority = Priority_HIGH
-	} else {
-		this.Message.Priority = Priority_NORMAL
-	}
-
-	return this
-}
-
-// SetCollapseKey This parameter identifies a group of messages
-// (e.g., with collapse_key: "Updates Available") that can be collapsed,
-// so that only the last message gets sent when delivery can be resumed.
-// This is intended to avoid sending too many of the same messages when the
-// device comes back online or becomes active (see delay_while_idle).
-func (this *FcmClient) SetCollapseKey(val string) *FcmClient {
-
-	this.Message.CollapseKey = val
-
-	return this
-}
-
-// SetNotificationPayload sets the notification payload based on the specs
-// https://firebase.google.com/docs/cloud-messaging/http-server-ref
-func (this *FcmClient) SetNotificationPayload(payload *NotificationPayload) *FcmClient {
-
-	this.Message.Notification = *payload
-
-	return this
-}
-
-// SetContentAvailable On iOS, use this field to represent content-available
-// in the APNS payload. When a notification or message is sent and this is set
-// to true, an inactive client app is awoken. On Android, data messages wake
-// the app by default. On Chrome, currently not supported.
-func (this *FcmClient) SetContentAvailable(isContentAvailable bool) *FcmClient {
-
-	this.Message.ContentAvailable = isContentAvailable
-
-	return this
-}
-
-// SetDelayWhileIdle When this parameter is set to true, it indicates that
-// the message should not be sent until the device becomes active.
-// The default value is false.
-func (this *FcmClient) SetDelayWhileIdle(isDelayWhileIdle bool) *FcmClient {
-
-	this.Message.DelayWhileIdle = isDelayWhileIdle
-
-	return this
-}
-
-// SetTimeToLive This parameter specifies how long (in seconds) the message
-// should be kept in FCM storage if the device is offline. The maximum time
-// to live supported is 4 weeks, and the default value is 4 weeks.
-// For more information, see
-// https://firebase.google.com/docs/cloud-messaging/concept-options#ttl
-func (this *FcmClient) SetTimeToLive(ttl int) *FcmClient {
-
-	if ttl > MAX_TTL {
-
-		this.Message.TimeToLive = MAX_TTL
-
-	} else {
-
-		this.Message.TimeToLive = ttl
-
-	}
-	return this
-}
-
-// SetRestrictedPackageName This parameter specifies the package name of the
-// application where the registration tokens must match in order to
-// receive the message.
-func (this *FcmClient) SetRestrictedPackageName(pkg string) *FcmClient {
-
-	this.Message.RestrictedPackageName = pkg
-
-	return this
-}
-
-// SetDryRun This parameter, when set to true, allows developers to test
-// a request without actually sending a message.
-// The default value is false
-func (this *FcmClient) SetDryRun(drun bool) *FcmClient {
-
-	this.Message.DryRun = drun
-
-	return this
 }
 
 // PrintResults prints the FcmResponseStatus results for fast using and debugging
@@ -351,10 +207,4 @@ func (this *FcmResponseStatus) IsTimeout() bool {
 func (this *FcmResponseStatus) GetRetryAfterTime() (t time.Duration, e error) {
 	t, e = time.ParseDuration(this.RetryAfter)
 	return
-}
-
-// SetCondition to set a logical expression of conditions that determine the message target
-func (this *FcmClient) SetCondition(condition string) *FcmClient {
-	this.Message.Condition = condition
-	return this
 }
